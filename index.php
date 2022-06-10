cf4d141df7714bd0dd345a988b218d13ab2cdff7
malikmuhammadazharuddin <malikmuhammadazharuddin@gmail.com>
2022-06-10 15:00:30 +0500

    Update index.php
    completed front page

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+     <title>CV Home</title>
+     <style>
+         body{
+             margin-left:10%;
+             margin-right:10%;
+         }
+         table{
+             width: 100%;
+         }
+     </style>
 </head>
 <body>
-    
+    <h1 align= "center">Muhaamad Azharuddin</h1>
+    <h3 align= "center">Student</h3>
+    <h5>About myself</h5>
+    <p>I am Muhammad Azharuddin Mali From Pakistan. I have completed my Matriculation from Government Boys High School Fakeerabad, and I have done my Fsc From Government Science College Gambat. Currently I am diong DIT at IBA IITC Gambat.</p>
+    <h3>Education</h3>
+    <table border="1">
+        <tr>
+            <th>Degree Title</th>
+            <th>Board/Institute</th>
+            <th>Grade</th>
+            <th>Year</th>
+            <th>Action</th>
+        </tr>
+    </table>
 </body>
 </html>
